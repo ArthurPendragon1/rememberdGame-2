e62b404d769cc7a36d1d5c925b0a3e165500d4b9
ArthurPendragon <kostyan.games.2002@gmail.com>
2024-03-30 09:57:11 +0300

    Project Version 0.1.1

---
+++ Source/rememberedGame/Private/GamePlayer2.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GamePlayer2.h"
+
+GamePlayer2::GamePlayer2()
+{
+}
+
+GamePlayer2::~GamePlayer2()
+{
+}
